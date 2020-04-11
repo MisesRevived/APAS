799d026341b1bf7f7dcc7b74b8e01e975e6ae643
Stefan <58218134+MisesRevived@users.noreply.github.com>
2020-04-11 20:12:14 +0200

    Add files via upload

---
+++ revInt.py
@@ -0,0 +1,21 @@
+"""
+APAS Problem 7: Reverse Integer 
+Description: "Given a 32-bit signed integer reverse digits of an integer." 
+Examples: "Input: 123, Output: 321"; "Input: -123, Output: -321", "Input: 120, Output: 21"
+""" 
+
+def revInt(int): 
+	strInt = str(int)
+	arrInt = []
+	for i in reversed(strInt):
+		if i != "0" and i != "-":
+			arrInt.append(i)
+		elif i == "-":
+			arrInt.insert(0, i)
+	strInt2 = ""
+	for k in arrInt:
+		strInt2 += k
+	return strInt2
+
+rev = revInt(120)
+print(rev)
